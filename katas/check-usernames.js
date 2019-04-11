eae7f707c4f62fd3d52886e0185bdd0b6cee4fc0
Anthony Medina <antmedina91@gmail.com>
2019-04-11 11:55:23 +0100

    replace test with correct template

--- katas/check-usernames.js
+++ katas/check-usernames.js
@@ -1,7 +1,11 @@
-const checkUsernames = require('../katas/check-usernames');
+/*
+  The function checkUsernames should take an array of usernames and return true if they are all valid and false if any are not valid.
+  A valid username:
+  - is at least 5 characters long
+  - may contain lowercase letters, numbers and underscores
+  - is no longer than 20 characters
+*/
 
-describe('checkUsernames', () => {
-  it('describe what happens if this test succeeds here', () => {
-    // expect(checkUsernames()).to...
-  });
-});
+function checkUsernames() {}
+
+module.exports = checkUsernames;
