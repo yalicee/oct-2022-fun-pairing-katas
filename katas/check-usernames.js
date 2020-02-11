1faf8b0440aa597c6ee09a435e3283f37dc1eb04
Paul Rogerson <paul.rogerson@northcoders.com>
2020-02-11 15:27:08 +0000

    Update check-usernames.js

--- katas/check-usernames.js
+++ katas/check-usernames.js
@@ -2,7 +2,7 @@
   The function checkUsernames should take an array of usernames and return true if they are all valid and false if any are not valid.
   A valid username:
   - is at least 5 characters long
-  - may contain lowercase letters, numbers and underscores
+  - may only contain lowercase letters, numbers and underscores
   - is no longer than 20 characters
 */
 
